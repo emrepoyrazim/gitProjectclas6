da574e73520b1c08e606a1eb6ca33051958ad5ba
emre <emrepoyrazim@gmail.com>
2022-10-20 22:44:39 +0300

    submain code 1

---
+++ src/test/java/pages/LoginPages.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class LoginPages {
+    // login code
+    public static void main(String[] args) {
+        System.out.println("login code");
+    }
+}
--- src/test/java/test/BaseCode.java
+++ src/test/java/test/BaseCode.java
@@ -1,3 +1,5 @@
+package test;
+
 public class BaseCode {
     public static void main(String[] args) {
         System.out.println("base code");
